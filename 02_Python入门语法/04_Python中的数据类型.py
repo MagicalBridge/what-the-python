1a377f960551dfa909a873de248426d48f757c10
chupengfei <450793585@qq.com>
2024-11-02 20:35:07 +0800

    feat: add type change

--- 02_Python入门语法/04_Python中的数据类型.py
+++ 02_Python入门语法/04_Python中的数据类型.py
@@ -1,6 +1,19 @@
 # 方式1：使用print直接输出类型信息
+print(type("字符串"))
+print(type(666))
+print(type(11.34))
 
 # 方式2：使用变量存储type()语句的结果
+string_type = type("第二种方式")
+int_type = type(666)
+float_type = type(11.345)
+
+print(string_type)
+print(int_type)
+print(float_type)
 
 # 方式3：使用type()语句，查看变量中存储的数据类型信息
+name = "第三种方式"
+name_type = type(name)
+print(name_type)
 
---
+++ 02_Python入门语法/05_Python中的数据类型转换.py
@@ -0,0 +1,13 @@
+# 将数字类型转换为字符串
+num_str = str(11)
+print(type(num_str), num_str)
+
+
+float_str = str(11.345)
+print(type(float_str),float_str)
+
+# 将字符串转换为数字
+num = int("11")
+print(type(num), num)
+
+
