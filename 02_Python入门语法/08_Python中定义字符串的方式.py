94e0c01af7e4331d080b4c8d30933d37eff18a30
chupengfei <450793585@qq.com>
2024-11-04 17:16:37 +0800

    feat: add Three ways of string

---
+++ 02_Python入门语法/08_Python中定义字符串的方式.py
@@ -0,0 +1,30 @@
+# 1、单引号
+single_quoted_string = '这是一个单引号字符串'
+print(single_quoted_string)
+
+# 2、双引号
+double_quoted_string = "这是一个双引号字符串"
+print(double_quoted_string)
+
+# 3、三重单引号
+triple_single_quoted_string = '''这是一个
+三重单引号字符串，
+可以跨越多行。'''
+print(triple_single_quoted_string)
+
+# 4、三重双引号
+triple_double_quoted_string = """这是一个
+三重双引号字符串，
+可以跨越多行。"""
+print(triple_double_quoted_string)
+
+# 5、原始字符串
+raw_string = r"这是一个原始字符串，\n不会被转义"
+print(raw_string)
+
+# 6、格式化字符串
+name = "Alice"
+age = 30
+formatted_string = f"我的名字是 {name}，我今年 {age} 岁。"
+print(formatted_string)
+
