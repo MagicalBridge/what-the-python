ba0ac7fd667452c896c655881d75355a988dc26f
chupengfei <450793585@qq.com>
2024-11-12 19:05:15 +0800

    feat: add boolean type

--- 02_Python入门语法/15_Python中的数据输入.py
+++ 02_Python入门语法/15_Python中的数据输入.py
@@ -6,3 +6,11 @@ print("请告诉我你是谁？")
 name = input()
 
 print("我知道你你是 %s" % name)
+
+
+# input 默认接收的类型是字符串类型，如果想要数字，需要手动转换一下：
+num = input("bank card password:")
+# 数据类型转换
+num = int(num)
+
+print("银行卡密码:", type(num))
---
+++ 03_Python中的判断语句/01_布尔类型和比较云算符.py
@@ -0,0 +1,12 @@
+"""
+演示布尔类型的定义
+以及比较运算符的应用
+"""
+
+# 定义变量存储布尔类型的数据
+bool_1 = True
+bool_2 = False
+print(f"boo_1变量的内容是：{bool_1}, 类型是：{type(bool_1)}")
+print(f"boo_2变量的内容是：{bool_2}, 类型是：{type(bool_2)}")
+
+# 比较运算符的使用
